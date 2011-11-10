f2e22065428f569ee04a34b2035519d304c6402d
Eugene Letuchy <eletuchy@fb.com>
2011-11-09 20:34:23 -0800

    [init] explicit custom file, comment package locations

--- init.el
+++ init.el
@@ -1,19 +1,33 @@
 
+(setq el-emacs-init-file load-file-name)
 (require 'cl)
 
-(setq marmalade-server "http://marmalade-repo.org/"
-      custom-file (expand-file-name "~/.emacs.d/custom.el"))
+;; backport user-emacs-directory variable to pre-24 versions of emacs
+(unless (boundp 'user-emacs-directory)
+  (defvar user-emacs-directory "~/.emacs.d/"
+    "Directory beneath which additional per-user
+    Emacs-specificfiles are placed. Various programs in Emacs
+    store information in this directory. Note that this should
+    end with a directory separator. See also
+    `locate-user-emacs-file`. "))
 
 ;; Packages
+(setq marmalade-server "http://marmalade-repo.org/")
+
 (require 'package)
-(dolist (source '(("marmalade" . "http://marmalade-repo.org/packages/")
-                  ("elpa" . "http://tromey.com/elpa/")
+(dolist (source '(
+                  ;; FSF only (official)
                   ("gnu" . "http://elpa.gnu.org/packages/")
+                  ;; User contributions
+                  ("marmalade" . "http://marmalade-repo.org/packages/")
+                  ;; Original package.el repo
+                  ("elpa" . "http://tromey.com/elpa/")
                   ))
   (add-to-list 'package-archives source t))
 (package-initialize)
 
-;; Obtaining packages: see the README for https://github.com/technomancy/emacs-starter-kit
+;; Obtaining packages: see the README for
+;; https://github.com/technomancy/emacs-starter-kit
 (when (not package-archive-contents)
   (package-refresh-contents))
 
@@ -36,3 +50,5 @@
   (when (not (package-installed-p p))
     (package-install p)))
 
+;; Customizations
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
